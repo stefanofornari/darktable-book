ce822628afc3cd043a219ba62fef00fb93872cb3
nicotranquil <nicotranquil@gmail.com>
2011-07-06 14:39:15 -0700

    Made a small edit to the build-manual.sh file

--- build-manual.sh
+++ build-manual.sh
@@ -4,7 +4,7 @@
 #you do not have the correct requirements installed or that the jodconverter
 #plug-in is not installed.
 #It is made by Nicholas Manea
-#This is covered under the Creative Commons Attribution-ShareAlike 3.0 Unported License (CC BY-SA 3.0)
+#This script is covered by no license
 
 soffice -accept="socket,host=127.0.0.1,port=8100;urp;" && mvn clean package
 echo "You may now close the recently opened OpenOffice window and this terminal window"
